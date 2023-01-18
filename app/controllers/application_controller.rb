e004cfe7fcf495389ec9993bdfb94a8924835763
Hikaru20210918 <hs2m.1022@gmail.com>
2023-01-18 22:02:36 +0900

    修正2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
- 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #if: :devise_controller?→deviseにまつわる画面に入った？Trueなら、configure_permitted_parametersメソッドを実行する
+
+ private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:profile,:position,:occupation])
+  end
 end
